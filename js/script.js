5fb57d331cb294988fe4142e2de9002ba81d0b5c
Dimka <arnoldzahar@gmail.com>
2020-11-18 16:40:11 +0100

    aktualisiert

--- js/script.js
+++ js/script.js
@@ -1,108 +1,91 @@
 "use strict";
 
-let  numberOfFilms;
 
-
-function start() {
-    numberOfFilms = prompt('How many films have you seen?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        /* console.log('error'); */
-        numberOfFilms = prompt('How many films have you seen?', '');
-    }
-}
-
-start();
 
 const pesonalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        pesonalMovieDB.count = prompt('How many films have you seen?', '');
+    
+        while (pesonalMovieDB.count == '' || pesonalMovieDB.count == null || isNaN(pesonalMovieDB.count)) {
+            /* console.log('error'); */
+            pesonalMovieDB.count = prompt('How many films have you seen?', '');
+        }
+    },
+
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('one of the last films you have seen?', ''),
+                  b = prompt('how would you review it?', '');
+        
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+                pesonalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }  
+            
+        }
+    },
+
+    detectPersonalLevel: function() {
+        if (pesonalMovieDB.count < 10) {
+            console.log('youve watched too little movies');
+        } else if (pesonalMovieDB.count >= 10 && pesonalMovieDB.count < 30) {
+            console.log('youre typical viwer');
+        } else if (pesonalMovieDB.count >= 30) {
+            console.log('youre cool viewer');
+        } else if (pesonalMovieDB.count == null) {
+            console.log('error');
+        } else {
+            console.log('error');
+        }
+    },
+
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(pesonalMovieDB);
+        }
+    },
+
+    toggleVisibleMyDB: function() {
+        if (pesonalMovieDB.privat == false) {
+            pesonalMovieDB.privat = true;
+        } else {
+            pesonalMovieDB.privat = false;
+        }
+    },
+
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            
+            let genre = prompt(`what is yor favourite genre number ${i}`, '');
+        
+        
+            if (genre != null && genre != ''  && genre.length < 50 ) {
+                pesonalMovieDB.genres[i - 1] = genre;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }  
+            
+        }
+        pesonalMovieDB.genres.forEach((item, i) => {
+            console.log(`Favourite genre ${i + 1} is ${item}`)
+        });
+        
+    },
+
+    
+
+
+
 };
-
-
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('one of the last films you have seen?', ''),
-              b = prompt('how would you review it?', '');
-    
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-            pesonalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }  
-        
-    }
-}
-
-/* rememberMyFilms(); */
-
-
-
-function detectPersonalLevel() {
-    if (pesonalMovieDB.count < 10) {
-        console.log('youve watched too little movies');
-    } else if (pesonalMovieDB.count >= 10 && pesonalMovieDB.count < 30) {
-        console.log('youre typical viwer');
-    } else if (pesonalMovieDB.count >= 30) {
-        console.log('youre cool viewer');
-    } else if (pesonalMovieDB.count == null) {
-        console.log('error');
-    } else {
-        console.log('error');
-    }
-}
-
-/* detectPersonalLevel(); */
-
-
-
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(pesonalMovieDB);
-    }
-}
-
-showMyDB(pesonalMovieDB.privat);
-
-/* function showMyDB() {
-    if (pesonalMovieDB.privat == false) {
-        console.log(pesonalMovieDB);
-    }
-}
-
-showMyDB(); */
-
-
-
-
-/* console.log(pesonalMovieDB); */
-
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`what is yor favourite genre number ${i}`, '');
-    
-    
-        if (genre != null && genre != ''  && genre.length < 50 ) {
-            pesonalMovieDB.genres[i - 1] = genre;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }  
-        
-    }
-}
-
-writeYourGenres();
 
--- js/scripttest.js
+++ js/scripttest.js
@@ -1,55 +1,186 @@
-"use strict";
+/* "use strict";
 
-let  numberOfFilms = +prompt('How many films have you seen?', '');
-
-let pesonalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('one of the last films you have seen?', ''),
-          b = prompt('how would you review it?', '');
-
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-        pesonalMovieDB.movies[a] = b;
-        console.log('done');
+options.makeTest();
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Eigenschafft ${i} ist ${options[key][i]}`);
+            counter++;
+        }
     } else {
-        console.log('error');
-        i--;
-    }    
+        console.log(`Eigenschafft ${key} ist ${options[key]}`);
+        counter++;
+    }
+    
 }
 
-while (numberOfFilms <= 10) {
-    console.log('youve watched too little movies');
-    numberOfFilms++;
-} while (numberOfFilms >= 10 && numberOfFilms < 30) {
-    console.log('youre typical viwer');
-    numberOfFilms++;
-} while (numberOfFilms >= 30) {
-    console.log('youre cool viewer');
-    numberOfFilms++;
-} 
-
-/* for (let i = 0; i < 10; i++) {
-    console.log('youve watched too little movies');
-} */
-
-/* if (pesonalMovieDB.count < 10) {
-    console.log('youve watched too little movies');
-} else if (pesonalMovieDB.count >= 10 && pesonalMovieDB.count < 30) {
-    console.log('youre typical viwer');
-} else if (pesonalMovieDB.count >= 30) {
-    console.log('youre cool viewer');
-} else if (pesonalMovieDB.count == null) {
-    console.log('error');
-} else {
-    console.log('error');
-} */
+console.log(counter);
 
 
-console.log(pesonalMovieDB);
+ */
+
+
+/* let text = 'what is your name',
+    msg = 'im sorry, but you didnt tell your name',
+    name = prompt(text);
+
+if (name != '') {
+    msg = 
+}
+
+document.write(msg);
+ */
+
+
+/* let text = 'what is your name',
+    name = prompt(text);
+
+    if (name == '') {
+        document.write('im sorry, but you didnt tell your name');
+    } else {
+        document.write("nice to mee you "+name+" !");
+    }
+
+     */
+
+
+    function copy(mainObj) {
+        let objCopy = {};
+
+        let key;
+        for (key in mainObj) {
+            objCopy[key] = mainObj [key];
+        }
+
+        return objCopy;
+     }
+
+    const numbers = {
+        a: 2,
+        b: 5,
+        c: {
+            x: 7,
+            y: 4
+        }
+    };
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add));
+
+
+
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'fghjk';
+console.log(newArray);
+console.log(oldArray);
+
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livej', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'fb']
+
+console.log(internet);
+
+
+
+function log(a, b, c, d) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+
+}
+
+const num = [2, 5, 7, 'huy'];
+
+log(...num);
+
+
+const array = ['a', 'b'];
+const newArrray = [...array];
+console.log(array);
+console.log(newArrray);
+
+
+const q = {
+    one: 1,
+    two: 2,
+    three: 3
+};
+
+const nq = {...q};
+
+nq.one = 5;
+
+console.log(q);
+console.log(nq);
+
+
+
+
+let str = "some";
+let strObj = new String(str);
+
+/* console.log(typeof(str));
+console.log(typeof(strObj)); */
+
+console.dir([1,2,3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('hello!');
+    }
+};
+
+const john = Object.create(soldier);
+
+john.health = 200;
+
+/* const john = {
+    health: 100
+}; */
+
+
+
+/* john.__proto__ = soldier; */
+
+
+/* Object.setPrototypeOf(john, soldier); */
+
+/* console.log(john.armor); */
+
+
+console.log(john);
+
+
+john.sayHello();
